c9fd03f437b983e9525447f98b41bcea80fb9cae
BenjaminS348 <coco.dragon002@outlook.com>
2024-04-11 18:59:34 +0200

    update some part

--- backend/app/api/controllers/safemoon/price.js
+++ backend/app/api/controllers/safemoon/price.js
@@ -13,6 +13,11 @@ module.exports = {
     .sort({Timestamp: -1})
     .exec()
     .then((result) => {
+      if (result.length === 0)
+        res.status(400).json({ msg: "Not found" });
+      else {
+        res.status(200).json({msg: "Found!", data: result[0]});
+      }
     })
     .catch(error =>  {
       res.status(400).json({ msg: "Not found" });
@@ -21,9 +26,6 @@ module.exports = {
   getPrices: function(req, res, next) {
     const startTime = new Date();
     startTime.setDate(startTime.getDate() - 7 + 1)
-    startTime.setHours(0);
-    startTime.setMinutes(0);
-    const current = new Date();
     priceModel.find({ 
       $and: [ { Timestamp: { $gte : startTime } }, { Timestamp: { $lte : current } }] 
     })
