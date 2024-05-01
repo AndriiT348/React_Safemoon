9d0f066b7dce0ccd208f9f0fb64497938cfe1b9f
BenjaminS348 <coco.dragon002@outlook.com>
2024-05-01 18:58:12 +0200

    some update

--- backend/app/api/controllers/safemoon/market.js
+++ backend/app/api/controllers/safemoon/market.js
@@ -2,7 +2,15 @@ const axios = require('axios');
 const marketModel = require('../../models/safemoon/market');	
 
 module.exports = {
-  
+  getMarket: async function(req, res, next) {
+    marketModel.find({}, function(err, result){
+      if (err){
+        res.status(400).json({ msg: "Not found" });
+      } else{				
+        res.status(200).json({msg: "Result found!", data: result});							
+      }
+    });
+  },
   create: function() {
     axios.get('https://api.coingecko.com/api/v3/coins/safemoon?tickers=true'+ new URLSearchParams({
       }))
@@ -10,8 +18,6 @@ module.exports = {
       await marketModel.remove({});
       await  Promise.all(result.data.tickers.forEach( async (item) => {
         let market = {};
-        market.name = `${item.market.name}(${item.target})`;
-        market.price = item.converted_last.usd;
         try {
           await marketModel.create(market);
           console.log(`success market of ${market.name} : ${market.price}`);
